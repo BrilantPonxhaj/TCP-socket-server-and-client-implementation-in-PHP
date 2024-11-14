0f942429eca98093b24b7fde42d36fd056b160a8
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-11-14 22:29:22 +0100

    Implemented socket connection logic in Client.php to enable communication with the server.

--- Client.php
+++ Client.php
@@ -1,6 +1,9 @@
 <?php
 
-class Client
-{
+$host = = '192.168.100.140';
+$port = 8080;
 
-}
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$result = socket_connect($socket, $host, $port);
+
+
