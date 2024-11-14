2a1fcad93a7d1f0d617f2014066009ff330a4f74
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-11-14 22:39:36 +0100

    Display connection status and error messages

--- Client.php
+++ Client.php
@@ -1,9 +1,18 @@
 <?php
 
+// Server IP and port
 $host = = '192.168.100.140';
 $port = 8080;
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 $result = socket_connect($socket, $host, $port);
+
+if (!$result) {
+       echo "Failed to connect to the server: " . socket_strerror(socket_last_error($socket)) . "\n";
+       exit();
+   }
+   
+   echo "Connected to the server at $host on port $port\n";
+   echo "Type your command or 'exit' to close the connection.\n";
 
 
