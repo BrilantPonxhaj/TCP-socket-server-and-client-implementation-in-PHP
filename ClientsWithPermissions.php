60fb21233f4eff691f960ba32107f3f8cce6ab3e
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-11-15 18:06:28 +0100

    Defined client list with permissions

--- ClientsWithPermissions.php
+++ ClientsWithPermissions.php
@@ -1,6 +1,34 @@
 <?php
 
-class ClientsWithPermissions
-{
-
-}
+$knownClients = [
+       [
+           'id' => 1,
+           'ip' => '',
+           'name' => 'Brikenda',
+           'readPermission' => true,
+           'writePermission' => true,
+           'executePermission' => true,
+       ],
+       [
+           'id' => 2,
+           'ip' => '',
+           'name' => 'Brineta',
+           'readPermission' => true,
+           'writePermission' => true,
+           'executePermission' => true,
+       ],
+       [
+           'id' => 3,
+           'ip' => '',
+           'name' => 'Brilant Ponxhaj',
+           'readPermission' => true,
+           'writePermission' => true,
+           'executePermission' => false,
+       ],
+       [
+           'id' => 4,
+           'ip' => '',
+           'name' => 'Çlirimtar',
+           
+       ],
+   ];
